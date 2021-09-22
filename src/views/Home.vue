811037584173ae93b0a382116c5ef172fdc45507
alexeypodlesnyy <alexeypodl@gmail.com>
2021-09-21 21:39:32 -0400

    Merged About page into Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,27 @@
 <template>
+<div>
+    <p>
+
+Clean geometric lines and gorgeous succulent plants come together in a modern style terrarium to be a focal piece in your home or office. Each piece of glass is hand cut, soldered, and finished with a smile in mind. Live plants are then arranged to make your perfect succulent garden.</p>
+    <img src="../assets/bg-masthead.jpg" class="img-fluid w-100">
   <div class="container">
-    <h1>This is a Home page</h1>
+    <div class="container-fluid">
+          <h1>Hi there, let’s get to know each other!</h1>
+    </div>
+    <div class="row">
+      <div class="col col-lg-8">
+        <img src="../assets/anushka.jpg" class="img-fluid">
+      </div>
+      <div class="col col-lg-4">
+        <h5>My name is Anna and I have a great interest for anything creative.</h5>
+        <h5>A researcher by day, I moonlight as a glass artist who blends nature and art in a shape of a geometric glass terrarium.</h5>
+        <h5>Each terrarium is handcrafted and unique - you will not find two identical compositions!</h5>
+        <h5>If you like a terrarium that is not in stock or would like to get a custom made one - please let me know and I will do my best to create it for you!</h5>
+      </div>
+
+    </div>
   </div>
+</div>
 </template>
 
 <script>
